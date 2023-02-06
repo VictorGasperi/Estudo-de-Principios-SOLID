0555a3b2132ce7d8d23695235412387b443da7c0
VictorGasperi <victorgasperi@gmail.com>
2023-02-06 01:47:30 -0300

    Aula28

---
+++ Módulo 2/Aula28/Aula28/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula28
+{
+    /// <summary>
+    /// Aula sobre composicao - Relacionameto do tipo "Tem um"
+    /// A parte nao existe sem o todo
+    /// Exemplo: mao e dedos.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
