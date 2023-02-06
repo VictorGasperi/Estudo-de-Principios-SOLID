1f4bf200c553586fd3998fd9fc1d1434671b22f9
VictorGasperi <victorgasperi@gmail.com>
2023-02-06 02:08:14 -0300

    .

--- Módulo 2/Aula29/Aula29/Program.cs
+++ Módulo 2/Aula29/Aula29/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Aula28
+{
+    /// <summary>
+    /// Aula sobre agregacao - Um tipo especial de composicao - portanto uma relacao do tipo "Tem um"
+    /// A diferenca entre composicao e agregacao, eh que na agregacao a parte pode existir sem o todo.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
